302f17ef44ba66f8eb34d1b36c0255df9fb1ef5d
FlorBro <florian.brochot33@gmail.com>
2026-01-29 09:08:20 +0100

    changing distance to 0.44

--- navigation/aruco_nav.py
+++ navigation/aruco_nav.py
@@ -35,7 +35,7 @@ class ArucoDocker:
 
         # --- REGLAGES UTILISATEUR ---
         self.marker_size = 0.164
-        self.target_dist = 0.60   # Distance finale souhaitée
+        self.target_dist = 0.44   # Distance finale souhaitée
         self.approach_dist = 0.90 # Distance de fin move_base / début servoing
         
         # CORRECTION DU DECALAGE "TROP A DROITE"
@@ -161,7 +161,7 @@ class ArucoDocker:
                 self.stop_robot()
         
         # Affichage
-        cv2.imshow("Docking View", cv_image)
+        #cv2.imshow("Docking View", cv_image)
         cv2.waitKey(3)
 
     def perform_search_scan(self):
